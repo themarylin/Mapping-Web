17f1696d1ec70f36d95ec09cf06647ace53bacd5
Mary Lin <25216651+themarylin90@users.noreply.github.com>
2018-09-20 17:30:18 -0700

    level 1 complete but still need a legend

---
+++ config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoidGhlbWFyeWxpbiIsImEiOiJjamx3ZHBkMGowdnljM3dsaWgyNzk2NnUxIn0.AI21Pex4MxPnQkSlhvAe_A";
---
+++ logic.js
@@ -0,0 +1,87 @@
+// Function to determine marker size based on population
+function markerSize(magnitude) {
+  return magnitude * 15000;
+}
+
+function markerColor(magnitude) {
+  if (magnitude > 5) {
+    return "red";
+  }
+  else if (magnitude > 4) {
+    return "orange";
+  }
+  else if (magnitude > 3) {
+    return "gold";
+  }
+  else if (magnitude > 2) {
+    return "yellow";
+  }
+  else if (magnitude > 1) {
+    return "GreenYellow";
+  }
+  else {
+    return "green";
+  }
+}
+
+//load data
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson", function (data) {
+  var places = data.features.map(d => d.properties.place);
+  var mags = data.features.map(d => d.properties.mag);
+  var locations = data.features.map(d => [d.geometry.coordinates[1],d.geometry.coordinates[0]]);
+  //console.log(locations);
+  // Define arrays to hold created markers for earthquakes
+  var cityMarkers = [];
+
+  // Loop through locations and create markers dependent on mag intensity
+  for (var i = 0; i < locations.length; i++) {
+    cityMarkers.push(
+      L.circle(locations[i], {
+        stroke: false,
+        fillOpacity: 0.75,
+        color: markerColor(mags[i]),
+        fillColor: markerColor(mags[i]),
+        radius: markerSize(mags[i])
+      })
+    )
+  };
+
+  console.log(cityMarkers);
+
+  // Define variables for our base layers
+  var lightmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.light",
+    accessToken: API_KEY
+  });
+
+  // Create a layer for cities
+  var cities = L.layerGroup(cityMarkers);
+
+  // Create a baseMaps object
+  var baseMaps = {
+    "Light Map": lightmap
+  };
+
+  // Create an overlay object
+  var overlayMaps = {
+    "City Earthquake Magnitudes": cities
+  };
+
+  // Define a map object
+  var myMap = L.map("map", {
+    center: [37.09, -95.71],
+    zoom: 5,
+    layers: [lightmap, cities]
+  });
+});
+
+
+
+
+// // Pass our map layers into our layer control
+// // Add the layer control to the map
+// L.control.layers(baseMaps, overlayMaps, {
+//   collapsed: false
+// }).addTo(myMap);
